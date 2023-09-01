c1ca0e8a9c30c775920cf4b4598294625e5f4139
abeda jarrah <Abeda-Aljarrah>
2023-09-01 18:46:25 +0300

    Moontak

---
+++ HTML/header.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" /> -->
+  <!-- <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" /> -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  <!-- <link href="../CSS/list.css" rel="stylesheet" /> -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Delicious+Handrawn&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
+  <title>Moontak</title>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link href="../CSS/header.css" rel="stylesheet" />
+
+</head>
+
+<body>
+  <!-- /********************************************NAV**********************************************/ -->
+  <div class="navbar">
+
+    <div class="logo">
+      <h2>Moontak</h2>
+    </div>
+
+
+    <ul class="links">
+      <li><a href="./index.php">Home</a></li>
+      <li><a href="./product-page.php">Shope</a></li>
+      <li><a href="./about-us.php">About</a></li>
+      <li><a href="./contact.php">Contact Us</a></li>
+      <li><a href="#">Blog</a></li>
+      <div>
+        <a href="../HTML/list.php">
+          <span class="material-symbols-outlined icon">shopping_cart</span> </a>
+        </div>
+        <div>
+          <a href="./profile.php">
+            <span class="material-symbols-outlined icon">account_circle</span>
+          </a>
+        </div>
+        <button class="signin"><a href="../HTML/sign-up.php">Login</a></button>
+    </ul>
+
+
+
+    <div class="toggle">
+      <div class="line1"></div>
+      <div class="line2"></div>
+      <div class="line3"></div>
+    </div>
+  </div>
+
+
+
+  <script>
+    const navbar = document.querySelector('.navbar')
+
+    navbar.querySelector('.toggle').addEventListener('click', () => {
+
+      navbar.classList.toggle('collapsed')
+
+    })
+
+
+
+    window.addEventListener('scroll', (e) => {
+
+      let windowY = window.pageYOffset
+
+      let navbarHeight = document.querySelector('.navbar').offsetHeight
+
+      if (windowY > navbarHeight) navbar.classList.add('sticky')
+      else navbar.classList.remove('sticky')
+
+
+
+
+    })
+  </script>
+</body>
+
+</html>
---
+++ HTML/index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  <link href="../CSS/index.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Delicious+Handrawn&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet" />
+
+  <title>Moontak</title>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="path/to/aos.js"></script>
+</head>
+
+<body>
+  <!-- /********************************************NAV**********************************************/ -->
+  <section>
+    <?php include './header.php' ?>
+  
+  
+    <div class="jumbotron jumbotron-bg text-center rounded-0" style="
+          background-image: url('../image/facebook-page-cover-820x312\ \(2\)\ \(1\)-modified\ \(1\).jpg');
+        ">
+      <div class="container">
+        <h1 class="pt-5 tracking-in-contract">Stay home we deliver</h1>
+      </div>
+    </div>
+
+  </section>
+
+
+  <section style="color: #365d5a;" >
+  <div class="container my-5">
+    <section id="steps">
+        <div class="text-center mb-5">
+            <h2 class="font-weight-bold display-4" style="color: #365d5a;" >How It <span style=" color: #f1895e">Works?</span></h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="bg-light position-relative px-3 my-5">
+                    <div class="font-weight-bold circle text-white rounded-circle d-flex align-items-center justify-content-center mx-auto position-relative border border-white"
+                         style="width: 60px;height: 60px;top: -30px;border-width: 4px !important; ">
+                         <img src="../image/How it works/2202250_account_avatar_human_man_profile_icon.png" class="how-image" />
+
+                    </div>
+                    <div class="px-3 text-center pb-3">
+                        <h4>Create an account</h4>
+                        <!-- <p class="font-weight-light my-3">Create an account</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="bg-light position-relative px-3 my-5">
+                    <div class="font-weight-bold circle text-white rounded-circle d-flex align-items-center justify-content-center mx-auto position-relative border border-white"
+                         style="width: 60px;height: 60px;top: -30px;border-width: 4px !important;">
+                         <img src="../image/How it works/3586362_checklist_document_list_paper_icon.png" class="how-image" />
+
+                    </div>
+                    <div class="px-3 text-center pb-3">
+                        <h4>Create your list by shopping</h4>
+                        <!-- <p class="font-weight-light my-3">Lorem ipsum dolor sit consectetur adipisicing elit. Alias amet
+                            deleniti et fugit iusto nesciunt.</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="bg-light position-relative px-3 my-5">
+                    <div class="font-weight-bold circle text-white rounded-circle d-flex align-items-center justify-content-center mx-auto position-relative border border-white"
+                         style="width: 60px;height: 60px;top: -30px;border-width: 4px !important;">
+                         <img src="../image/How it works/7114165_application_notification_subscription_subscribe_alarm_icon.png" class="how-image" />
+
+                    </div>
+                    <div class="px-3 text-center pb-3">
+                        <h4>Choose a plane</h4>
+                        <!-- <p class="font-weight-light my-3">Lorem ipsum dolor sit consectetur adipisicing elit. Alias amet
+                            deleniti et fugit iusto nesciunt.</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="bg-light position-relative px-3 my-5">
+                    <div class="font-weight-bold circle text-white rounded-circle d-flex align-items-center justify-content-center mx-auto position-relative border border-white"
+                         style="width: 60px;height: 60px;top: -30px;border-width: 4px !important;">
+                         <img src="../image/How it works/9876797_calendar_calendar date_shipping_delivery_logistics_icon.png" class="how-image" />
+
+                    </div>
+                    <div class="px-3 text-center pb-3">
+                        <h4>Choose the days/period you want to deliver your list</h4>
+                        <!-- <p class="font-weight-light my-3">Lorem ipsum dolor sit consectetur adipisicing elit. Alias amet
+                            deleniti et fugit iusto nesciunt.</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="bg-light position-relative px-3 my-5">
+                    <div class="font-weight-bold circle text-white rounded-circle d-flex align-items-center justify-content-center mx-auto position-relative border border-white"
+                         style="width: 60px;height: 60px;top: -30px;border-width: 4px !important;">
+                         <img src="../image/How it works/3586361_location_map_navigation_pointer_icon.png" class="how-image" />
+
+                    </div>
+                    <div class="px-3 text-center pb-3">
+                        <h4>Add a correct location</h4>
+                        <!-- <p class="font-weight-light my-3">Lorem ipsum dolor sit consectetur adipisicing elit. Alias amet
+                            deleniti et fugit iusto nesciunt.</p> -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="bg-light position-relative px-3 my-5">
+                    <div class="font-weight-bold circle text-white rounded-circle d-flex align-items-center justify-content-center mx-auto position-relative border border-white"
+                         style="width: 60px;height: 60px;top: -30px;border-width: 4px !important;">
+                         <img src="../image/How it works/9027005_shopping_cart_thin_icon.png" class="how-image" />
+
+                    </div>
+                    <div class="px-3 text-center pb-3">
+                        <h4>Enjoy your journey with us!</h4>
+                        <!-- <p class="font-weight-light my-3">Lorem ipsum dolor sit consectetur adipisicing elit. Alias amet
+                            deleniti et fugit iusto nesciunt.</p> -->
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+</div>
+    <!-- <div class="how-it-works">
+      <a href="#">
+        <img src="../image/How it works/2202250_account_avatar_human_man_profile_icon.png" class="how-image" />
+        <h3 class="category-title">Create an account</h3>
+      </a>
+      <a href="#">
+        <img src="../image/How it works/3586362_checklist_document_list_paper_icon.png" class="how-image" />
+        <h3 class="category-title">Create your list by shopping</h3>
+      </a>
+      <a href="#">
+        <img src="../image/How it works/7114165_application_notification_subscription_subscribe_alarm_icon.png" class="how-image" />
+        <h3 class="category-title">Choose a plane</h3>
+      </a>
+      <a href="#">
+        <img src="../image/How it works/9876797_calendar_calendar date_shipping_delivery_logistics_icon.png" class="how-image" />
+        <h3 class="category-title">
+          Choose the days/period
+          <p>you want to deliver your list</p>
+        </h3>
+      </a>
+      <a href="#">
+        <img src="../image/How it works/3586361_location_map_navigation_pointer_icon.png" class="how-image" />
+        <h3 class="category-title">Add a correct location</h3>
+      </a>
+      <a href="#">
+        <img src="../image/How it works/9027005_shopping_cart_thin_icon.png" class="how-image" />
+        <h3 class="category-title">Enjoy your journey with us!</h3>
+      </a>
+    </div> -->
+  </section>
+
+
+
+  <section class="cat-sec">
+    <div class="col-md-12">
+      <div class="section-header d-flex justify-content-between mb-5">
+        <h2 class="section-title">Our Categories</h2>
+      </div>
+    </div>
+
+    <div class="category-carousel swiper swiper-initialized swiper-horizontal">
+      <div class="swiper-wrapper" id="swiper-wrapper-fac3d4d32a28dfbd" aria-live="polite">
+        <a href="category.html" class="bounce-in-fwd">
+          <img src="../image/icons/barbecue.png" class="category-image" />
+          <h3 class="category-title">Meat & Poultry</h3>
+        </a>
+        <a href="category.html" class="bounce-in-fwd">
+          <img src="../image/icons/dairy.png" class="category-image" />
+          <h3 class="category-title">Dairy</h3>
+        </a>
+        <a href="../HTML/product-page.php" class="bounce-in-fwd">
+          <img src="../image/icons/healthy-food (2).png" class="category-image" />
+          <h3 class="category-title">Fruits & Vegetables</h3>
+        </a>
+        <a href="category.html" class="bounce-in-fwd">
+          <img src="../image/icons/cleaning.png" class="category-image" />
+          <h3 class="category-title">Cleaning & Household</h3>
+        </a>
+        <a href="category.html" class="bounce-in-fwd">
+          <img src="../image/icons/shopping-cart.png" class="category-image" />
+          <h3 class="category-title">Food Cupboard</h3>
+        </a>
+        <a href="category.html" class="bounce-in-fwd">
+          <img src="../image/icons/baby-products (1).png" class="category-image" />
+          <h3 class="category-title">Baby Products</h3>
+        </a>
+      </div>
+    </div>
+  </section>
+
+
+  <div class="our-plans">
+    <h2 class="section-title">Our Plans</h2>
+    <div class="card-containar fade-in-top" data-aos="fade-up">
+      <div class="card hidden" style="
+            background-image: url('../image/no-revisions-ixS7UCRJTdM-unsplash-modified.jpg');
+          ">
+        <h2 class="card-title">Monthly Plane</h2>
+        <button class="card-button">Learn more</button>
+      </div>
+      <div class="card hidden" style="
+            background-image: url('../image/ginny-rose-stewart-_kUCAyYFVBI-unsplash-modified.jpg');
+          ">
+        <h2 class="card-title">Quarter Plane</h2>
+        <button class="card-button">Learn more</button>
+      </div>
+      <div id="yearly" class="card hidden" style="
+            background-image: url('../image/Premium Photo _ Cotton eco friendly shopping bag full of fresh organic vegetables and grocery products on grey table-modified.jpg');
+          ">
+        <h2 class="card-title">Yearly Plane</h2>
+        <button class="card-button">Learn more</button>
+      </div>
+    </div>
+  </div>
+
+  
+
+  
+
+  
+
+  <section id="testimonials" >
+    <!--heading--->
+    <div class="testimonial-heading">
+      <h2 class="section-title">Feedback</h2>
+    </div>
+    <!--testimonials-box-container------>
+    <div class="testimonial-box-container">
+      <!--BOX-1-------------->
+      <div class="testimonial-box">
+        <!--top------------------------->
+        <div class="box-top">
+          <!--profile----->
+          <div class="profile">
+            <!--img---->
+            <div class="profile-img">
+              <img src="../image/persons/ian-dooley-d1UPkiFd04A-unsplash.jpg" />
+            </div>
+            <!--name-and-username-->
+            <div class="name-user">
+              <strong>Liam mendes</strong>
+              <span>@liammendes</span>
+            </div>
+          </div>
+          <!--reviews------>
+          <div class="reviews">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="far fa-star"></i><!--Empty star-->
+          </div>
+        </div>
+        <!--Comments---------------------------------------->
+        <div class="client-comment">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, quaerat quis? Provident temporibus architecto
+            asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+            tenetur voluptates incidunt blanditiis sed atque cumque.
+          </p>
+        </div>
+      </div>
+      <!--BOX-2-------------->
+      <div class="testimonial-box">
+        <!--top------------------------->
+        <div class="box-top">
+          <!--profile----->
+          <div class="profile">
+            <!--img---->
+            <div class="profile-img">
+              <img src="../image/persons/rachel-mcdermott-0fN7Fxv1eWA-unsplash.jpg" />
+            </div>
+            <!--name-and-username-->
+            <div class="name-user">
+              <strong>Noah Wood</strong>
+              <span>@noahwood</span>
+            </div>
+          </div>
+          <!--reviews------>
+          <div class="reviews">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i><!--Empty star-->
+          </div>
+        </div>
+        <!--Comments---------------------------------------->
+        <div class="client-comment">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, quaerat quis? Provident temporibus architecto
+            asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+            tenetur voluptates incidunt blanditiis sed atque cumque.
+          </p>
+        </div>
+      </div>
+      <!--BOX-3-------------->
+      <div class="testimonial-box">
+        <!--top------------------------->
+        <div class="box-top">
+          <!--profile----->
+          <div class="profile">
+            <!--img---->
+            <div class="profile-img">
+              <img src="../image/persons/stephanie-liverani-Zz5LQe-VSMY-unsplash.jpg" />
+            </div>
+            <!--name-and-username-->
+            <div class="name-user">
+              <strong>Oliver Queen</strong>
+              <span>@oliverqueen</span>
+            </div>
+          </div>
+          <!--reviews------>
+          <div class="reviews">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="far fa-star"></i><!--Empty star-->
+          </div>
+        </div>
+        <!--Comments---------------------------------------->
+        <div class="client-comment">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem, quaerat quis? Provident temporibus architecto
+            asperiores nobis maiores nisi a. Quae doloribus ipsum aliquam
+            tenetur voluptates incidunt blanditiis sed atque cumque.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section style="margin: 50px;">
+    <div class="section-header">
+      <h2 class="section-title suppliers-title">Suppliers</h2>
+    </div>
+    <div class="Suppliers">
+      <img src="../image/Suppliers/327425817_576396794017077_4140454578082634265_n-removebg-preview.png" alt="Rakan" class="supp-image" />
+      <img src="../image/Suppliers/340631960_748992923552285_952954235911564382_n-removebg-preview (1).png" alt="Sameh mole" class="supp-image" />
+      <img src="../image/Suppliers/المراعي-removebg-preview (1).png" alt="al-razy" class="supp-image" />
+      <img src="../image/Suppliers/channels4_profile-removebg-preview.png" alt="Kasih" class="supp-image" />
+      <img src="../image/Suppliers/logo.png" alt="Nabeel" class="supp-image" />
+      <!-- <img src="/image/Suppliers/المراعي-removebg-preview (1).png" alt="maraee" class="supp-image"> -->
+    </div>
+  </section>
+
+  <?php include './footer.php' ?>
+
+
+  <script src="../JS/index.js"></script>
+  <!-- <script>
+    AOS.init();
+  </script> -->
+</body>
+
+</html>
